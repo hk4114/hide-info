53ad79cb23ed391ea561402fce8fda006f756e52
hk4114 <741180788@qq.com>
2019-04-15 22:24:17 +0800

    :tada: init package of hide-info

---
+++ README.md
@@ -0,0 +1,9 @@
+# hide-info
+
+## How to use?
+```
+const HIDE = require('hide-info');
+const fun = '1234567898765'
+const b = HIDE(fun,3,3)
+console.log(b) // 123*******765
+```
---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = (param,left=0,right=0)=>{
+    const len = param.length
+    const r = new RegExp("^(.{"+left+"})(?:\\d+)(.{"+right+"})$","gim");
+    let c = "";
+    for(let i=0;i<len-left-right;i++){
+      c+="*"
+    }
+    return param.replace(r, "$1"+c+"$2");
+}
